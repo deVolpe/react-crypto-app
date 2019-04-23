618f4aca1a4477e654424df2f5406c467d25d71b
deVolpe <deVolpe@users.noreply.github.com>
2019-04-23 21:22:10 +0300

    fix: fix conditions

--- validators/login.js
+++ validators/login.js
@@ -7,13 +7,13 @@ module.exports = function validateLoginInput(data) {
   data.email = !isEmpty(data.email) ? data.email : '';
   data.password = !isEmpty(data.password) ? data.password : '';
 
-  if (!Validator.isEmpty(data.email)) {
+  if (Validator.isEmpty(data.email)) {
     errors.email = 'Email is required';
   }
   if (!Validator.isEmail(data.email)) {
     errors.email = 'Email is invalid';
   }
-  if (!Validator.isEmpty(data.password)) {
+  if (Validator.isEmpty(data.password)) {
     errors.password = 'Password is required';
   }
   if (!Validator.isLength(data.password, { min: 6, max: 16 })) {
--- validators/register.js
+++ validators/register.js
@@ -10,23 +10,23 @@ module.exports = function validateRegisterInput(data) {
     ? data.passwordConfirm
     : '';
 
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email is required';
+  }
   if (!Validator.isEmail(data.email)) {
     errors.email = 'Email is invalid';
   }
-  if (!Validator.isEmpty(data.email)) {
-    errors.email = 'Email is required';
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'Password is required';
   }
   if (!Validator.isLength(data.password, { min: 6, max: 16 })) {
-    errors.password = 'Password must have 6 chars';
+    errors.password = 'Password must have a range from 6 to 16 chars';
   }
-  if (!Validator.isEmpty(data.password)) {
-    errors.password = 'Password is required';
+  if (Validator.isEmpty(data.passwordConfirm)) {
+    errors.passwordConfirm = 'This input is required';
   }
   if (!Validator.equals(data.password, data.passwordConfirm)) {
     errors.passwordConfirm = 'Passwords are not match';
-  }
-  if (!Validator.isEmpty(data.passwordConfirm)) {
-    errors.passwordConfirm = 'This input is required';
   }
 
   return { errors, isValid: isEmpty(errors) };
